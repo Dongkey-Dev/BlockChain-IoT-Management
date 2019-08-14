aaeb04be67008630e5ef63580d205f1efe156c5b
yunut <somewheres@nate.com>
2019-08-15 03:49:17 +0900

    iot_recent_comp_19_08_15

--- app/src/main/java/d/somewheres/uieosio/NetworkManageActivity.java
+++ app/src/main/java/d/somewheres/uieosio/NetworkManageActivity.java
@@ -2,29 +2,38 @@ package d.somewheres.uieosio;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class NetworkManageActivity extends AppCompatActivity {
 
     ListView listview; //리스트뷰 객체 생성
-    ListViewAdapter adapter; //어댑터 생성
+    ListViewAdapterIoTtime adapter; //어댑터 생성
     String networkname; //네트워크 이름을 받아온다.
     String account;
     HTTPThread httpThread;
@@ -63,7 +72,7 @@ public class NetworkManageActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         // 여기 리스트뷰에서는 최근 조작한 IoT장치 값 받아서 나타냄(미완)
-        adapter = new ListViewAdapter();
+        adapter = new ListViewAdapterIoTtime();
         listview = (ListView) findViewById(R.id.listview2);
         listview.setAdapter(adapter);
         //아이템 클릭시 iot장치의 이름 넘겨준다.
@@ -183,17 +192,120 @@ public class NetworkManageActivity extends AppCompatActivity {
 
             @Override
             protected void onPostExecute(String s) {
+                String iottitle;
+                String iotdata;
+                String iottime;
+                List<String> device = new ArrayList<>();
+                HashMap<String, List<List>> result = new HashMap<String, List<List>>();
                 Search_Json sj = new Search_Json();
-                List ss = sj.Parsing_Response(s,"attachdevice");
-                String a = String.valueOf(ss);
+                result = sj.Lookup_device_detail(s);
+                device.addAll(result.keySet());
+                for (int i = 0 ; i < result.keySet().size() ; i++) {
+                    iottitle = device.get(i);
+                    iotdata = result.get(device.get(i)).get(0).get(0).toString();
+                    iottime = result.get(device.get(i)).get(0).get(1).toString();
+                    adapter.addItem(ContextCompat.getDrawable(NetworkManageActivity.this, R.drawable.iot), iottitle, iotdata, iottime);
+                    adapter.notifyDataSetChanged();
+                }
                 super.onPostExecute(s);
-                TextView test = (TextView)findViewById(R.id.networkID);
-                test.setText(a);
+
+
+                TextView controllist = (TextView)findViewById(R.id.controllist);
+                controllist.setVisibility(View.GONE);
+
+
             }
         }
 
 
     }
+    //리스트뷰에 값을 넣어줄수 있도록 도와주는 어댑터이다.
+    public class ListViewAdapterIoTtime extends BaseAdapter {
+        // Adapter에 추가된 데이터를 저장하기 위한 ArrayList
+        private ArrayList<ListViewIoTtime> listViewItemList = new ArrayList<ListViewIoTtime>() ;
 
+        // ListViewAdapter의 생성자
+        public ListViewAdapterIoTtime() {
+
+        }
+
+        // Adapter에 사용되는 데이터의 개수를 리턴
+        @Override
+        public int getCount() {
+            return listViewItemList.size() ;
+        }
+
+        // position에 위치한 데이터를 화면에 출력하는데 사용될 View를 리턴
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            final int pos = position; //클릭한 리스트의 위치
+            final Context context = parent.getContext(); //부모 컨택스트를 얻어와 뷰를 사용
+
+            // "listview_item" Layout을 inflate하여 convertView 참조 획득.
+            if (convertView == null) {
+                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = inflater.inflate(R.layout.listview_iottime, parent, false);
+            }
+
+            // 화면에 표시될 View(Layout이 inflate된)으로부터 위젯에 대한 참조 획득
+            ImageView iconImageView = (ImageView) convertView.findViewById(R.id.iotimage) ;
+            TextView titleTextView = (TextView) convertView.findViewById(R.id.iottitle) ;
+            TextView descTextView = (TextView) convertView.findViewById(R.id.iotdata) ;
+            TextView timeTextView = (TextView) convertView.findViewById(R.id.iottime) ;
+
+            // Data Set(listViewItemList)에서 position에 위치한 데이터 참조 획득
+            ListViewIoTtime listViewItem = listViewItemList.get(position);
+
+            // 아이템 내 각 위젯에 데이터 반영
+            iconImageView.setImageDrawable(listViewItem.getIconDrawable());
+            titleTextView.setText(listViewItem.getTitleIoT());
+            descTextView.setText(listViewItem.getData());
+            timeTextView.setText(listViewItem.getTime());
+
+            return convertView;
+        }
+
+        // 지정한 위치(position)에 있는 데이터와 관계된 아이템(row)의 ID를 리턴. : 필수 구현
+        @Override
+        public long getItemId(int position) {
+            return position ;
+        }
+
+        // 지정한 위치(position)에 있는 데이터 리턴 : 필수 구현
+        @Override
+        public Object getItem(int position) {
+            return listViewItemList.get(position) ;
+        }
+
+        //리스트뷰 지정된 위치에있는 이름을 얻어올수 있다.
+        public String getname(int position) { return listViewItemList.get(position).getTitleIoT();}
+        // 아이템 데이터 추가를 위한 함수. 개발자가 원하는대로 작성 가능.
+        public void addItem(Drawable icon, String title, String desc, String time) {
+            ListViewIoTtime item = new ListViewIoTtime();
+
+            item.setIconDrawable(icon);
+            item.setTitleIoT(title);
+            item.setData(desc);
+            item.setTime(time);
+
+            listViewItemList.add(item);
+        }
+
+        //cursor를 이용해서 리스트뷰에 아이템 추가
+        public void addItem(Drawable icon, Cursor cursor) {
+
+            while (cursor.moveToNext()) {
+                ListViewIoTtime item = new ListViewIoTtime();
+                item.setIconDrawable(icon);
+                item.setTitleIoT(cursor.getString(1));
+                item.setData(cursor.getString(2));
+                item.setTime(cursor.getString(3));
+
+
+                listViewItemList.add(item);
+            }
+            cursor.close();
+        }
+    }
 
 }
